bc673de94388769a9c05a874f41c8aede3ef568c
EC2 Default User <ec2-user@ip-172-31-80-202.ec2.internal>
2023-03-10 16:59:17 +0000

    Refactor fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,17 +1,13 @@
 def fizzbuzz(number)
-    if number == 3
-        return 'fizz'
-    elsif number == 5
-        return 'buzz'
-    elsif number % 5 == 0 && number % 3 != 0
-        return 'buzz'
-    elsif number % 3 == 0 && number % 5 != 0
-        return 'fizz'
-    elsif number % 3 == 0 || number % 5 == 0
+    if number % 3 == 0 && number % 5 == 0
         return 'fizzbuzz'
+    elsif number % 5 == 0 #&& number % 3 != 0
+        return 'buzz'
+    elsif number % 3 == 0 #&& number % 5 != 0
+        return 'fizz'
     else
-        return number.to_s
+        return number.to_s # string for continuity
     end
 end
 
-puts fizzbuzz(30)
+puts fizzbuzz(15)
